d6c7f63261666381fae437529ba83606d0bb33bd
iManuhK <161714108+iManuhK@users.noreply.github.com>
2024-05-13 09:27:02 +0300

    adds to simple fetch lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,18 @@
 // create your App component here
+import React,{useEffect,useState} from 'react'
+
+function App() {
+    const [dog,setDogs] = useState([])
+useEffect(()=>{
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then(res=>res.json())
+    .then(data=>setDogs(data))
+},[])
+
+  return (<div>
+{!dog ? <p>Loading...</p> : <img src={dog.message} alt="A Random Dog" /> }
+  </div>
+    )
+}
+
+export default App
